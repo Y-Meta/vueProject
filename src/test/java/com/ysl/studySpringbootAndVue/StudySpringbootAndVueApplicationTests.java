cfc61a137ca214331032ab68b0ba4fc971bb4fca
Y-meta <y_meta@163.com>
2020-08-01 20:00:38 +0800

    int

---
+++ src/test/java/com/ysl/studySpringbootAndVue/StudySpringbootAndVueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysl.studySpringbootAndVue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringbootAndVueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
